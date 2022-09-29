2dcdd8865f70dc97e57c268acc50698a4c816386
Vigneshwaran7708 <wvignesh671@gmail.com>
2022-09-29 14:49:57 +0530

    librecontoller,setting

---
+++ LibraryManagementAPI/LibraryManagement/LibregController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementAPI.LibraryManagement
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Libreg : ControllerBase
+    {
+        [HttpPost("{id}")]
+        public string Post(LibregEntity entity)
+        {
+            try
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@sno", Convert.ToString(entity.sno)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@yourname", entity.yourname));
+                sqlParameters.Add(new KeyValuePair<string, string>("@youremail ", entity.youremail));
+                sqlParameters.Add(new KeyValuePair<string, string>("@password ", entity.password));
+                sqlParameters.Add(new KeyValuePair<string, string>("@repeatpassword ", entity.repeatpassword));
+                var result = manageSQL.InsertData("insertintolibrarianreg", sqlParameters);
+                return JsonConvert.SerializeObject(result);
+            }
+            catch (Exception ex)
+            {
+                //AuditLog.WriteError(ex.Message);
+            }
+            return "false";
+        }
+    }
+    public class LibregEntity
+    {
+        public int sno { get; set; }
+        public string yourname { get; set; }
+        public string youremail { get; set; }
+
+        public string password { get; set; }
+
+        public string repeatpassword { get; set; }
+
+    }
+}
---
+++ LibraryManagementAPI/LibraryManagement/SettingController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementAPI.LibraryManagement
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class setting : ControllerBase
+    {
+        [HttpPost("{id}")]
+        public string Post(SettingEntity entity)
+        {
+            try
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+                List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
+                sqlParameters.Add(new KeyValuePair<string, string>("@sno", Convert.ToString(entity.sno)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@firstname", entity.firstname));
+                sqlParameters.Add(new KeyValuePair<string, string>("@lastname  ", entity.lastname));
+                sqlParameters.Add(new KeyValuePair<string, string>("@email ", entity.email));
+                sqlParameters.Add(new KeyValuePair<string, string>("@phoneno ", Convert.ToString(entity.phoneno)));
+                sqlParameters.Add(new KeyValuePair<string, string>("@oldpassword ", entity.oldpassword));
+                sqlParameters.Add(new KeyValuePair<string, string>("@newpassword ", entity.newpassword));
+                sqlParameters.Add(new KeyValuePair<string, string>("@confirmpassword ", entity.confirmpassword));
+
+                var result = manageSQL.InsertData("Insertintosettings", sqlParameters);
+                return JsonConvert.SerializeObject(result);
+            }
+            catch (Exception ex)
+            {
+                //AuditLog.WriteError(ex.Message);
+            }
+            return "false";
+        }
+    }
+    public class SettingEntity
+    {
+        public int sno { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+
+        public string email { get; set; }
+
+        public int phoneno { get; set; }
+
+        public string oldpassword { get; set; }
+
+        public string newpassword { get; set; }
+
+        public string confirmpassword { get; set; }
+
+    }
+}
