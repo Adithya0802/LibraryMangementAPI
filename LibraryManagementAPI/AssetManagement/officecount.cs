a144b37bbdedaeee127579819ea12ba1fd57953a
ananth61 <ananth.murugan16@gmail.com>
2022-11-23 11:26:56 +0530

    Office psp

---
+++ LibraryManagementAPI/AssetManagement/officecount.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace LibraryManagementAPI.AssetManagement
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class officecount : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            ds = manageSQL.GetDataSetValues("Getofficecount");
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+}
+
+
---
+++ LibraryManagementAPI/AssetManagement/printercount.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace LibraryManagementAPI.AssetManagement
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class printercount : Controller
+    {
+
+
+        [HttpGet]
+        public string Get()
+        {
+            ManageSQLConnection manageSQL = new ManageSQLConnection();
+            DataSet ds = new DataSet();
+            ds = manageSQL.GetDataSetValues("Getprintercount");
+            return JsonConvert.SerializeObject(ds.Tables[0]);
+        }
+    }
+}
+
+
